7ca6cdf0fc5a0af8062ac3fb241de3e3a34b2986
Awais Akram <awaisakram182@gmail.com>
2025-04-12 12:12:54 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/SoundEthic../",
+  base:"/SoundEthics-/",
   plugins: [react(),tailwindcss(),],
 })
